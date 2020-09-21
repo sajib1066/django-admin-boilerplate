3bfe5a3a677d378a0e519d437a39837426c5113d
Sajib Hossain <sajib1066@gmail.com>
2020-09-21 21:16:03 +0600

    move all view in pages app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from .views import HomePageView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomePageView.as_view(), name='home'),
+    path('', include('pages.urls')),
 ]
