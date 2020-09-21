e3df5fa586da2bcef90fe2b51886bf4d0e6299ed
Sajib Hossain <sajib1066@gmail.com>
2020-09-21 21:46:48 +0600

    add analytical dashboard

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import (
-    HomePageView
+    HomePageView,
+    AnalyticalPageView
 )
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('analytical/', AnalyticalPageView.as_view(), name='analytical'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+
+class AnalyticalPageView(TemplateView):
+    template_name = 'dashboard/analytical.html'
