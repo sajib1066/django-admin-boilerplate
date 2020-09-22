bd11c6be38fe7cb24d8f930c6e385802bbc60e4e
Sajib Hossain <sajib1066@gmail.com>
2020-09-22 17:04:51 +0600

    delete pages app

--- pages/views/authentication.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class Login1View(TemplateView):
-    template_name = 'authentication/login1.html'
--- pages/views/dashboard.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class HomePageView(TemplateView):
-    template_name = 'index.html'
-
-
-class AnalyticalPageView(TemplateView):
-    template_name = 'dashboard/analytical.html'
