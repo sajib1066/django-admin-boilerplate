994d475256ca28d90b8779cae8dcc7a0f5c9dff1
Sajib Hossain <sajib1066@gmail.com>
2020-09-23 12:35:55 +0600

    set allowed host

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^ubierb^a800l2$ra2t-mun#o+tb$&v*@9ch=hej-b&qxw284t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
